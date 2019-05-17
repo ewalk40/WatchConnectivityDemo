2fd7ede03319209a76e6590b230306e0c84568e5
Eric Walker <walkerkid@me.com>
2019-05-17 17:04:03 -0400

    Initial Commit

---
+++ WatchConnectivityDemo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchConnectivityDemo WatchKit Extension
+//
+//  Created by Eric Walker on 5/17/19.
+//  Copyright © 2019 Eric Walker. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ WatchConnectivityDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchConnectivityDemo
+//
+//  Created by Eric Walker on 5/17/19.
+//  Copyright © 2019 Eric Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
